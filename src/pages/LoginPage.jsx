5f1c12d1842e84024ca5c033d3cb6fae99111f6c
deepdsi <desaideep006@gmail.com>
2025-04-28 15:25:24 +0530

    update

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -38,36 +38,22 @@ const LoginPage = () => {
         );
         navigate("/"); // Redirect to Home after successful login
       } else {
-        dispatch(
-          setLogin({
-            user: loggedIn.user,
-            token: loggedIn.token,
-          })
-        );
-        navigate("/"); // R
         // If login failed, show error message
-        // setError(loggedIn.message || "Login failed. Please try again.");
+        setError(loggedIn.message || "Login failed. Please try again.");
 
-        // // Clear the error message after 2 seconds
-        // setTimeout(() => {
-        //   setError("");  // Reset error message
-        // }, 2000);
+        // Clear the error message after 2 seconds
+        setTimeout(() => {
+          setError("");  // Reset error message
+        }, 2000);
       }
     } catch (err) {
-      dispatch(
-        setLogin({
-          user: loggedIn.user,
-          token: loggedIn.token,
-        })
-      );
-      navigate("/"); // R
-      // console.log("Login failed", err.message);
-      // setError("Login failed. Please try again.");
+      console.log("Login failed", err.message);
+      setError("Login failed. Please try again.");
 
-      // // Clear the error message after 2 seconds
-      // setTimeout(() => {
-      //   setError("");  // Reset error message
-      // }, 2000);
+      // Clear the error message after 2 seconds
+      setTimeout(() => {
+        setError("");  // Reset error message
+      }, 2000);
     }
   };
 
